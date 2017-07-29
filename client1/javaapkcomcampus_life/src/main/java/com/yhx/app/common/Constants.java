8da78ab56eb6104894a095db961eb7a6888aab2d
hangh <hangh@gionee.com>
2017-07-29 08:00:26 +0800

    handsecond

---
+++ client1/javaapkcomcampus_life/src/main/java/com/yhx/app/common/Constants.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.yhx.app.common;
+
+/**
+ * @author yhx
+ *
+ */
+public class Constants {
+	/** 与服务器端连接的协议名 */
+	public static final String PROTOCOL = "http://";
+	/** 服务器IP */
+	public static final String HOST = "101.201.238.86";//AVD test
+	/** 服务器端口号 */
+	public static final String PORT = "8080";
+	public static final String URL = PROTOCOL+HOST+":"+PORT; 
+}
